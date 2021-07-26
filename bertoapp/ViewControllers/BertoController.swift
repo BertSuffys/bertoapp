957734f19b08d3a331e2d36803aa7b3b9855b893
Unknown <unknown@example.com>
2021-07-26 14:27:50 +0200

    hallo

---
+++ bertoapp/ViewControllers/BertoController.swift
@@ -0,0 +1,21 @@
+//
+//  BertoController.swift
+//  bertoapp
+//
+//  Created by Berto on 26/07/2021.
+//
+
+import Foundation
+import UIKit
+
+class BertoController : UIViewController {
+    @IBOutlet weak var testKnop: UIButton!
+    
+    override func viewDidLoad() {
+        
+    }
+    func btnpress() {
+        
+    }
+    
+}
